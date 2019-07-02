f31537878db62c75e3937345989f6e0841c96a48
shaillenkumar <shaillenkumar@gmail.com>
2019-07-02 10:33:43 +0530

    Added ES6 Excercise

---
+++ ES6/letconst.js
@@ -0,0 +1,21 @@
+var x = 10;
+{ 
+    let x = 2;
+    console.log("1 -> "+x)
+}
+console.log("2 -> "+x);
+
+{
+    var y = 100;
+}
+
+console.log("3-> "+y)
+
+var i = 10;
+{ 
+    const i = 2;
+    console.log("4-> "+i)
+}
+const pi = 3.14;
+//  pi = 9.8;
+console.log("pi-> "+pi)
