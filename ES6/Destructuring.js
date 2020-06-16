c4e0ac09b68e9c62703ab85fbbc585a1eaac8100
Shailendra kumar <shailendra@Shailendras-MacBook-Pro.local>
2020-06-16 08:35:16 +0530

    Object Destructuring

--- ES6/Destructuring.js
+++ ES6/Destructuring.js
@@ -3,3 +3,19 @@ const numbers = [1,2,3];
 console.log(num1 , num2);
 [x, y, z] = numbers;
 console.log(x , z);
+
+//object destructuring
+{
+    const person = {name:"Ajay", age:22, location:"Mumbai"}
+    let name = person.name;
+    let age = person.age;
+    let location = person.location;
+    console.log(name , age ,location);
+}
+{
+    const person = {name:"Arvind", age:26, location:"New Delhi"}
+    
+    let {name,age,location} = person;
+    console.log(name , age ,location);
+
+}
